41d313844f3c8d949c23c4167cda4b2b4b032470
shinosnipe <shinosnipe@qq.com>
2023-01-23 21:38:41 +0800

    2.62作业完成

--- homework/2.62/main.c
+++ homework/2.62/main.c
@@ -8,8 +8,19 @@
 #include "../include/clib.h"
 
 
+int int_shifts_are_arithmetic()
+{
+	int x = ~0x0;
+	int shifts = (sizeof(int)<<3)-1;
+	x >>= shifts;
+	return !(~x & 0xF0);
+}
+
+
 int main()
 {
+	printf("该机器是否使用算术右移： %d\n", int_shifts_are_arithmetic());
+
 	return 0;
 }
 
