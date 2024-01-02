a74e71c0901c11f0e06ad645c5a4e212635f0622
shinosnipe <shinosnipe@qq.com>
2024-01-02 23:25:35 +0800

    格式更新

--- homework/include/clib.h
+++ homework/include/clib.h
@@ -33,159 +33,135 @@ void print2bitl(long x);
 /*******************************************************************************
  * Function name: itoa
  * Description: int转换二进制char字符
- * @param n	要转换成二进制的int
- * @param s	返回二进制char的数组
+ * @param n  要转换成二进制的int
+ * @param s  返回二进制char的数组
  ******************************************************************************/
-void itoa(int n, char s[])
-{
-	int i;
-	for (i = 0; i < sizeof(int)*8; i++)
-	{
-		s[i] = (n&0x1) + '0';
-		n = n >> 1;
-	}
+void itoa(int n, char s[]) {
+  int i;
+  for (i = 0; i < sizeof(int)*8; i++) {
+    s[i] = (n&0x1) + '0';
+    n = n >> 1;
+  }
 
-	// 末尾添加结尾符
-	s[i] = '\0';
-	reverse(s);
+  // 末尾添加结尾符
+  s[i] = '\0';
+  reverse(s);
 }
 
 
 /*******************************************************************************
  * Function name: reverse
  * Description: 反转字符数组
- * @param s	要反转的字符数组
+ * @param s  要反转的字符数组
  ******************************************************************************/
-void reverse(char s[])
-{
-	int len = strlen(s);
+void reverse(char s[]) {
+  int len = strlen(s);
 
-	int i, j;
-	for (i = 0, j = len-1; i < j; i++, j--)
-	{
-		char temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-	}
+  int i, j;
+  for (i = 0, j = len-1; i < j; i++, j--) {
+    char temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
 }
 
 
 /*******************************************************************************
  * Function name: ltoa
  * Description: 
- * @param s	要反转的字符数组
+ * @param s  要反转的字符数组
  ******************************************************************************/
-void ltoa(long n, char s[])
-{
-	char ctmp[80] = {0};
-	itoa(n, ctmp);
-	itoa(n>>(sizeof(int)<<3), s);
-	strcat(s, ctmp);
+void ltoa(long n, char s[]) {
+  char ctmp[80] = {0};
+  itoa(n, ctmp);
+  itoa(n>>(sizeof(int)<<3), s);
+  strcat(s, ctmp);
 }
 
 
-void print2bitl(long x)
-{
-	char s[80] = {};
-	ltoa(x, s);
-	int i = 0;
-	printf("%c", s[i]);
-	for (i = 1; s[i] != '\0'; i++)
-	{
-		if (i%4 == 0)
-		{
-			printf(" ");
-		}
-		if (i%8 == 0)
-		{
-			printf(" ");
-		}
-		if (i%16 == 0)
-		{
-			printf(" ");
-		}
-		if (i%32 == 0)
-		{
-			printf(" ");
-		}
-		printf("%c", s[i]);
-	}
-	printf("\n");
+void print2bitl(long x) {
+  char s[80] = {};
+  ltoa(x, s);
+  int i = 0;
+  printf("%c", s[i]);
+  for (i = 1; s[i] != '\0'; i++) {
+    if (i%4 == 0) {
+      printf(" ");
+    }
+    if (i%8 == 0) {
+      printf(" ");
+    }
+    if (i%16 == 0) {
+      printf(" ");
+    }
+    if (i%32 == 0) {
+      printf(" ");
+    }
+    printf("%c", s[i]);
+  }
+  printf("\n");
 }
 
 
 /*******************************************************************************
  *
  ******************************************************************************/
-void print2bit(int x)
-{
-	char s[40] = {};
-	itoa(x, s);
-	int i = 0;
-	printf("%c", s[i]);
-	for (i = 1; s[i] != '\0'; i++)
-	{
-		if (i%4 == 0)
-		{
-			printf(" ");
-		}
-		if (i%8 == 0)
-		{
-			printf(" ");
-		}
-		if (i%16 == 0)
-		{
-			printf(" ");
-		}
-		if (i%32 == 0)
-		{
-			printf(" ");
-		}
-		printf("%c", s[i]);
-	}
-	printf("\n");
+void print2bit(int x) {
+  char s[40] = {};
+  itoa(x, s);
+  int i = 0;
+  printf("%c", s[i]);
+  for (i = 1; s[i] != '\0'; i++) {
+    if (i%4 == 0) {
+      printf(" ");
+    }
+    if (i%8 == 0) {
+      printf(" ");
+    }
+    if (i%16 == 0) {
+      printf(" ");
+    }
+    if (i%32 == 0) {
+      printf(" ");
+    }
+    printf("%c", s[i]);
+  }
+  printf("\n");
 }
 
 
 /*******************************************************************************
  * Function name: show_bytes
  * Description: 打印十六进制的字节
- * @param start	打印开始地址
- * @param len	打印长度
+ * @param start  打印开始地址
+ * @param len  打印长度
  ******************************************************************************/
-void show_bytes(byte_pointer start, size_t len)
-{
-	size_t i;
-	for (i = 0; i < len; i++)
-		printf(" %.2x", start[i]);
-	printf("\n");
+void show_bytes(byte_pointer start, size_t len) {
+  size_t i;
+  for (i = 0; i < len; i++)
+    printf(" %.2x", start[i]);
+  printf("\n");
 }
 
-void show_pointer(void *x)
-{
-	show_bytes((byte_pointer) &x, sizeof(void *));
+void show_pointer(void *x) {
+  show_bytes((byte_pointer) &x, sizeof(void *));
 }
 
-void show_float(float x)
-{
-	show_bytes((byte_pointer) &x, sizeof(float));
+void show_float(float x) {
+  show_bytes((byte_pointer) &x, sizeof(float));
 }
 
-void show_short(short x)
-{
-	show_bytes((byte_pointer) &x, sizeof(short));
+void show_short(short x) {
+  show_bytes((byte_pointer) &x, sizeof(short));
 }
-void show_int(int x)
-{
-	show_bytes((byte_pointer) &x, sizeof(int));
+void show_int(int x) {
+  show_bytes((byte_pointer) &x, sizeof(int));
 }
-void show_long(long x)
-{
-	show_bytes((byte_pointer) &x, sizeof(long));
+void show_long(long x) {
+  show_bytes((byte_pointer) &x, sizeof(long));
 }
-void show_double(double x)
-{
-	show_bytes((byte_pointer) &x, sizeof(double));
+void show_double(double x) {
+  show_bytes((byte_pointer) &x, sizeof(double));
 }
 
 
@@ -193,34 +169,31 @@ void show_double(double x)
  * Function name: is_little_endian
  * Description: 判断机器是否是小端
  * @return 返回小端是否值
- * 	-1 机器是小端
- * 	-0 机器是大端
+ *   -1 机器是小端
+ *   -0 机器是大端
  ******************************************************************************/
-int is_little_endian()
-{
-	int16_t a = 0x1234;
-	int8_t b = ((byte_pointer) &a)[0];
+int is_little_endian() {
+  int16_t a = 0x1234;
+  int8_t b = ((byte_pointer) &a)[0];
 
-	if (b == 0x34)
-	{
-		return 1;
-	}
+  if (b == 0x34) {
+    return 1;
+  }
 
-	return 0;
+  return 0;
 }
 
 
 /*******************************************************************************
  * Function name: replace_byte
  * Description: 替换指定字节
- * @param[in] x	目标操作的字
+ * @param[in] x  目标操作的字
  * @param[in] i 替换字节的位置，以低位0到高位编号
  * @param[in] b 替换的内容字节
  * @return 返回替换过的x
  ******************************************************************************/
-unsigned replace_byte(unsigned x, int i, unsigned char b)
-{
-	((byte_pointer)&x)[i] = b;
-	return x;
+unsigned replace_byte(unsigned x, int i, unsigned char b) {
+  ((byte_pointer)&x)[i] = b;
+  return x;
 }
 
